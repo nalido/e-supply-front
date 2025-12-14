e830b2b2e9c2bef398185ca8e8f4787137b3db1f
nalido_mac_m1 <nalido@mac.m1>
2025-12-14 13:57:55 +0800

    fix: add back button icon import

--- src/views/SampleDetail.tsx
+++ src/views/SampleDetail.tsx
@@ -24,6 +24,7 @@ import type { ColumnsType } from 'antd/es/table';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import {
   EyeOutlined,
+  ArrowLeftOutlined,
   DownloadOutlined,
   EditOutlined,
   FileOutlined,
